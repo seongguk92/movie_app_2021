892911a6b8c37e410a7fad52cf14392b1d358e1b
seongguk <live8681@naver.com>
2021-04-26 17:28:22 +0900

    food exam

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+
+function Food({name, picture}){
+  return <div>
+    <h2>I like {name}</h2>
+    <img src={picture} />
+  </div>
+}
+
+const foodILike = [
+  {
+    name:"Kimchi",
+    image:
+      "https://www.bing.com/images/blob?bcid=S6XFCkyejawCqxcxoNWLuD9SqbotqVTdP.E"
+  },
+  {
+    name:"Kimbap",
+    image:
+      "https://recipe1.ezmember.co.kr/cache/recipe/2016/02/21/f34c2f0fcd67513941d683d90050f3c01.jpg"
+  },
+
+];
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {{foodILike.map(dish => (
+       <Food name={dish.name} picture={dish.image}/>
+      ))}}
     </div>
-  );
+  )
 }
 
 export default App;

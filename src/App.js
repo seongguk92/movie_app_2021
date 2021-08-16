dff68a08ae6c989fbfd474bf4afb8daed95f2fea
seongguk <live8681@naver.com>
2021-08-16 19:28:11 +0900

    study

--- src/App.js
+++ src/App.js
@@ -1,34 +1,33 @@
 import React from "react"
+  
+class App extends React.Component{
+  state = {
+    count: 0
+  };
 
-function Food({name, picture}){
-  return <div>
-    <h2>I like {name}</h2>
-    <img src={picture} />
-  </div>
-}
+  add = () => {
+    this.setState(current => ({count: current.count + 1}));
+  };
+  minus = () => {
+    this.setState(current => ({count: current.count - 1}));
+  };
 
-const foodILike = [
-  {
-    name:"Kimchi",
-    image:
-      "https://www.bing.com/images/blob?bcid=S6XFCkyejawCqxcxoNWLuD9SqbotqVTdP.E"
-  },
-  {
-    name:"Kimbap",
-    image:
-      "https://recipe1.ezmember.co.kr/cache/recipe/2016/02/21/f34c2f0fcd67513941d683d90050f3c01.jpg"
-  },
+  componentDidMount(){
 
-];
+  }
+  componentDidUpdate(){
+    
+  }
 
-function App() {
-  return (
-    <div>
-      {{foodILike.map(dish => (
-       <Food name={dish.name} picture={dish.image}/>
-      ))}}
-    </div>
-  )
+  render(){
+    return(
+      <div>
+        <h1>The number is: {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )
+  }
 }
 
 export default App;

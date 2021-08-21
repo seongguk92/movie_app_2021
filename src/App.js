c8f718d04114686738509203ec20b5057b3a3f06
seongguk <live8681@naver.com>
2021-08-21 23:03:29 +0900

    #5.0 update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import React from "react";
 import axios from "axios";
 import Movie from "./Movie";
-  
-class App extends React.Component{
-  state ={
+import "./App.css";
+
+class App extends React.Component {
+  state = {
     isLoading: true,
     movies: []
   };
 
   getMovies = async () => {
     const {
-      data: { 
-        data :{movies}
+      data: {
+        data: { movies }
       }
     } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
     this.setState({ movies, isLoading: false })
   };
 
-  async componentDidMount(){
+  async componentDidMount() {
     // setTimeout(() => {
     //   this.setState({ isLoading: false});
     // }, 6000);
@@ -25,22 +26,37 @@ class App extends React.Component{
     this.getMovies();
   };
 
-  render(){
+  render() {
     const { isLoading, movies } = this.state;
     // return <div>{isLoading ? "Loading...": "We are ready"}</div>;
-    return <div>{isLoading ? "Loading...": movies.map(movie => {
-      return (
-      <Movie 
-      key={movie.id}
-          id={movie.id} 
-          year={movie.year} 
-          title={movie.title} 
-          summary={movie.summary} 
-          poster={movie.medium_cover_image} 
-      />
-    );
-    })}
-    </div>;
+    return (
+      <section className="container">
+        {isLoading
+          ? <div className="loader">
+            <span className="loader__text">
+              "Loading..."
+            </span>
+          </div>
+          : (
+            <div className="movies">
+              {
+                movies.map(movie => (
+                  <Movie
+                    key={movie.id}
+                    id={movie.id}
+                    year={movie.year}
+                    title={movie.title}
+                    summary={movie.summary}
+                    poster={movie.medium_cover_image}
+                    genres={movie.genres}
+                  />
+                ))
+              }
+
+
+            </div>
+          )}
+      </section>);
   };
 }
 
